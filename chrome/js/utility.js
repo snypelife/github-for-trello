af50deced74663a984db49ddf793cab716c52992
snypelife <brettmrogerson@gmail.com>
2016-06-27 00:30:07 -0400

    simplify code and introduce lodash

--- chrome/js/utility.js
+++ chrome/js/utility.js
@@ -1,45 +1,9 @@
 'use strict';
 
-function is(target, state) {
-  switch (state.replace(/\?/g, '')) {
-    case 'hidden':
-      return target.offsetWidth === target.offsetHeight == 0;
-    case 'visible':
-      return target.offsetWidth > 0 < target.offsetHeight;
-    case 'empty':
-      return Object.keys(target).length === 0;
-    case 'object':
-      return /object/i.test(target.constructor.name);
-    case 'string':
-      return /string/i.test(target.constructor.name);
-    case 'array':
-      return Array.isArray(target);
-    case 'function':
-      return /function/i.test(target.constructor.name);
-  }
+export function isHidden(target) {
+  return target.offsetWidth === target.offsetHeight === 0;
 }
 
-function pick(target, props) {
-  const dest = {};
-
-  if (is(props, 'string?')) {
-    props = [props];
-  }
-
-  props.forEach((key) => {
-    dest[key] = target[key];
-  })
-
-  return dest;
+export function isVisible(target) {
+  return target.offsetWidth > 0 < target.offsetHeight;
 }
-
-function filter(target, iterator) {
-  if (!is(target, 'array?') || !is(iterator, 'function?')) { return []; }
-  return target.filter(iterator);
-}
-
-function startsWith(target, substring) {
-  return target.indexOf(substring) === 0;
-}
-
-export { is, pick, filter, startsWith }
